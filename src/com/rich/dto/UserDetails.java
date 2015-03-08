8bf3702d074a10dfa18f82fced0eeb55db23f0db
rbgwozdz <richard.gwozdz@gmail.com>
2015-03-08 00:33:26 -0500

    Initial Hibernate 10

---
+++ src/com/rich/dto/UserDetails.java
@@ -0,0 +1,38 @@
+package com.rich.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USER_DETAILS")
+public class UserDetails {
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private int userId;
+	private String userName;
+	private Address address;
+	
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
--- src/com/rich/hibernate/HibernateTest.java
+++ src/com/rich/hibernate/HibernateTest.java
@@ -4,13 +4,22 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.rich.dto.Address;
+import com.rich.dto.UserDetails;
+
 public class HibernateTest {
 
 	public static void main(String[] args) {
 		UserDetails user = new UserDetails();
-		user.setId(3);
-		user.setUserName("ThirdUsers");
+		user.setUserName("SecondUser");
 		
+		Address address = new Address();
+		address.setStreet("Street");
+		address.setCity("city");
+		address.setState("state");
+		address.setZipcode("00000");
+		
+		user.setAddress(address);
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
